6081f8b9dc1b1e81f42e2016ead4ae7faff255f1
Živorad Milekić <zivoradmilekic@gmail.com>
2018-07-13 19:10:22 +0200

    Added support for admin-bar in frontend

--- functions.php
+++ functions.php
@@ -178,3 +178,9 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 // ini_set('error_reporting', 0);
+
+add_action('get_header', 'remove_admin_login_header');
+
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
